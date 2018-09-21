4edfbd5e6ba08d998bcdd22a2d4eaf37eb238be0
nakkajima11 <42951256+nakkajima11@users.noreply.github.com>
2018-09-21 10:24:09 +0900

    cpm

--- src/cp.sh
+++ src/cp.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+
 ppass=0     #playerのパスの数
 cpass=0     #cpmのパスの数
 order=''    #先攻後攻
 other=''    #もう片方
 secret=0
 s=0
+pf=0
+cf=0
 pd=();
 cpd=();
 
@@ -18,15 +21,15 @@ done
 echo ${a[@]}
 }
 
-
 Order(){
 s=$(($RANDOM % 2 + 1))
 
 if [ $s -lt 2 ];then
         order='Player'
-
+        other='CPM'
         else
         order='CPM'
+        other='Player'
 fi
 
 echo 'ゲーム開始 : 先行' $order
@@ -34,24 +37,12 @@ echo 'ゲーム開始 : 先行' $order
 echo "$order : ${a[0]}"
 pd=(${a[0]} ${pd[@]})
 a=(${a[@]:1})
-echo "$other : ${a[0]}"
+echo "$other : ?"
 secret=${a[0]}
-a=(${a[@]:1}
+a=(${a[@]:1})
 cpsum=$((cpsum + secert))
 }
-Pdrow(){
-read -p '>>>> カードを引きますか？ d/p:' choose
 
-if [ $choose = 'd' ];then
-        pd=(${pd[@]} ${a[0]})
-        playersum=$((playersum + {a[0]}))
-        echo 'Player : '"${pd[@]} = 合計"  $playersum
-        a=(${a[@]:1})
-
-        else
-        ppass=$((ppass+1))
-fi
-}
 
 Cdrow(){
 if [ $cpsum -le 17 ];then
@@ -66,7 +57,7 @@ if [ $cchoose = 'd' ];then
         cpd=(${cpd[@]} ${a[0]})
         echo "${a[0]}"
         echo '合計 : ? +' "${cpd[@]}"
-        cpsum=$((cpsum + {a[0]}))
+        cpsum=$(( cpsum + ${a[0]} ));
         a=(${a[@]:1})
 
         else
@@ -75,7 +66,7 @@ fi
 }
 Check(){
 if [ $cpsum -le 21 -a $playersum -le 21 ];then
-        if [ $cpsum < $playersum ];then
+        if [ $cpsum -lt $playersum ];then
                 echo 'CPM : ' $cpsum
                 echo 'Player : ' $playersum
                 echo 'You Win!'
@@ -84,6 +75,16 @@ if [ $cpsum -le 21 -a $playersum -le 21 ];then
                 echo 'CPM : ' $cpsum
                 echo 'Player : ' $playersum
                 echo 'You Lose'
+        fi
+elif [ $cpsum -le 21 ];then
+                echo 'CPM : '$cpsum
+                echo 'Player : '$playersum
+                echo 'You Lose'
+
+        else
+        echo 'You Win!'
+
+fi
 }
 
 Drow
@@ -92,10 +93,25 @@ while [ $ppass -lt 2 -a $cpass -lt 2 ];
 do
 if [ $s -lt 2 ];then
         Pdrow
+        pf=1
+        else
+        Cdrow
+        cf=1
+fi
+
+if [ $pf -lt 1 ];then
+        Pdrow
+        cf=0
 
         else
         Cdrow
+        pf=0
 fi
+
 done
+
 Check
+
+
+
 
